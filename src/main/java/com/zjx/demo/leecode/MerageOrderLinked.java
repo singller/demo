dbef89b98602fdff625896e334de75fd62166e9f
zhangjianxun <zhangjianxun@credit-first.com.cn>
2020-08-06 09:26:17 +0800

    fix:有效的括号   合并顺序链表

---
+++ src/main/java/com/zjx/demo/leecode/MerageOrderLinked.java
@@ -0,0 +1,71 @@
+package com.zjx.demo.leecode;
+
+/**
+ * @author: create by zhangjianxun
+ * @version: v1.0
+ * @description: com.zjx.demo.leecode
+ *
+ * 两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * @date:2020/8/6
+ **/
+public class MerageOrderLinked {
+
+    /*
+    * 递归算法
+    * 时间O(m+n)
+    * 空间(m+n)
+    * */
+    public ListNode1 mergeTwoLists(ListNode1 l1, ListNode1 l2) {
+        if(l1 == null){
+            return l2;
+        }else if(l2==null){
+            return l1;
+        }
+        //如果l1小于l2
+        if(l1.val<l2.val){
+            //l1得下一个节点指向重新判断得  l2跟l1.next  结果
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l2.next,l1);
+            return l2;
+        }
+    }
+
+
+    /*
+     * 迭代算法
+     * 时间O(m+n)
+     * 空间(1)
+     * */
+    public ListNode1 mergeTwoLists1(ListNode1 l1, ListNode1 l2) {
+        //初始哨兵节点
+        ListNode1 preHead = new ListNode1(-1);
+
+        ListNode1 pre = preHead;
+        while (l1 != null && l2 != null){
+            if(l1.val<=l2.val){
+                pre.next = l1;
+                l1 = l1.next;
+            }else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        //判断剩下的不为空的节点
+        pre.next = l1==null?l2:l1;
+        return preHead.next;
+    }
+}
+
+
+class ListNode1 {
+     int val;
+     ListNode1 next;
+     ListNode1() {}
+     ListNode1(int val) { this.val = val; }
+     ListNode1(int val, ListNode1 next) { this.val = val; this.next = next; }
+ }
---
+++ src/main/java/com/zjx/demo/leecode/VaildBracket.java
@@ -0,0 +1,70 @@
+package com.zjx.demo.leecode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author: create by zhangjianxun
+ * @version: v1.0
+ * @description: com.zjx.demo.leecode 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 实例
+ * 输入: "()"
+ * 输出: true
+ *
+ * @date:2020/8/4
+ **/
+public class VaildBracket {
+
+    public Map<Character,Character> map = new HashMap<Character,Character>();
+
+    public VaildBracket(){
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+    }
+
+
+    public boolean solution1(String str){
+
+        if(str.length()%2 != 0){
+            return false;
+        }
+        String replace = "";
+        while (str.contains("{}") || str.contains("[]") || str.contains("()")){
+            str = str.replace("{}","");
+            str = str.replace("[]","");
+            str = str.replace("()","");
+        }
+        return "".equals(str);
+    }
+
+    public boolean solution2(String str){
+        Stack<Character> stack = new Stack<>();
+        //遍历字符串
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            //判断map中是否包含此元素
+            if(map.containsKey(c)){
+                //判断栈是否为空
+                char topElement = stack.empty() ? '#' : stack.pop();
+                //判断map中值是否跟栈中值相等
+                if(topElement != map.get(c)){
+                    return false;
+                }
+            }else {
+                //否则进行压栈
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+}
