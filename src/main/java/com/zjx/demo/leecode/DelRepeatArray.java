5b0fe8b3dc4d82fcc7727b593d0b1c8e4e5bd878
zhangjianxun <zhangjianxun@credit-first.com.cn>
2020-08-10 17:15:52 +0800

    fix:阻塞队列实现   两数和 删除重复数组

---
+++ src/main/java/com/zjx/demo/leecode/DelRepeatArray.java
@@ -0,0 +1,47 @@
+package com.zjx.demo.leecode;
+
+/**
+ * @author: create by zhangjianxun
+ * @version: v1.0
+ * @description: com.zjx.demo.leecode
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ *
+ * 给定数组 nums = [1,1,2],
+ *
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ *
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ **/
+public class DelRepeatArray {
+
+    /*
+    * 双指针法
+    * */
+    public int removeDuplicates(int[] nums) {
+
+        if(nums.length == 0 ) return 0;
+        //先遍历循环数组
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            //判断两个指针元素值是否相等
+            if(nums[i] != nums[j]){
+                //不相等  则把后边元素相前移动
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
---
+++ src/main/java/com/zjx/demo/leecode/TwoTogether.java
@@ -0,0 +1,46 @@
+package com.zjx.demo.leecode;
+
+/**
+ * @author: create by zhangjianxun
+ * @version: v1.0
+ * @description: com.zjx.demo.leecode
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ **/
+public class TwoTogether {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode node = new ListNode(0);
+        ListNode p = l1,q = l2, curr =node;
+        int carry = 0;
+        //两个链表都不为空
+        while (p!= null || q!=null){
+            int i = (p != null) ? p.val : 0;
+            int j = (q != null) ? q.val : 0;
+            int sum = carry + i+j;
+            carry = sum /10;
+            curr.next = new ListNode(sum %10);
+            curr = curr.next;
+
+            if(p!=null) p=p.next;
+            if(q!=null) q=q.next;
+        }
+        if(carry >0){
+            curr.next = new ListNode(carry);
+        }
+        return node.next;
+    }
+
+}
